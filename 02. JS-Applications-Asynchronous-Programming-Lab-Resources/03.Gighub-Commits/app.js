6a3a6f92700bab3121c9571d87b18535ec1c0c24
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-03 15:57:53 +0100

    Added Solution for 03.Github-Commits

--- 02. JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
+++ 02. JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
@@ -1,4 +1,24 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+  const username = document.getElementById("username").value;
+  const repoName = document.getElementById("repo").value;
+
+  const commits = document.getElementById("commits");
+  commits.innerHTML = "";
+
+  try {
+    const response = await fetch(`https://api.github.com/repos/${username}/${repoName}/commits`);
+    if (response.ok == false) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+    const data = await response.json();
+    
+    data.forEach(x => {
+        const liElement=document.createElement('li');
+        liElement.textContent= `${x.commit.author.name}: ${x.commit.message}`;
+        commits.appendChild(liElement)
+    })
+   
+} catch (error) {
+    console.log("Error", error);
+  }
 }
