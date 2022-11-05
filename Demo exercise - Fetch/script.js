217a695108353ddadb71825a510751ac2b87edd4
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-05 12:04:50 +0100

    Added solution for Fake Store Api

---
+++ Demo exercise - Fetch/script.js
@@ -0,0 +1,20 @@
+const url = "https://fakestoreapi.com/products";
+
+fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    const cardItem = document.getElementById("cards");
+    data.forEach(x=>{
+// console.log(x)
+        cardItem.innerHTML += `<div class="card">
+                  <h1 class="card__title">${x.title}</h1>
+                 <img src=${x.image} alt="img" class="card__img">
+                 <p>Description:${x.description}</p>
+                  <p class="card__category">${x.category}</p>
+                  <p class="card__price">${x.price}</p>
+             </div>`;
+    })
+ })
+  .catch((err) => {
+    console.log(err);
+  });
