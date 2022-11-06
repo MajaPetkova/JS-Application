996b311e460289bc2920d2ba6617b161a3ee0b40
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-06 23:08:07 +0100

    Update app.js
    resolving mistakes

--- 04.JS App-Data-and-Auth-Exercise/05.Fisher Game/src/app.js
+++ 04.JS App-Data-and-Auth-Exercise/05.Fisher Game/src/app.js
@@ -1,9 +1,16 @@
 window.addEventListener("DOMContentLoaded", () => {
   const userData = JSON.parse(sessionStorage.getItem("userData"));
   if (userData != null) {
-    document.getElementById("user").style.display = "none";
-    document.querySelector("#addForm .add").disabled = false;
-  } else {
-    document.getElementById("guest").style.display = "none";
-  }
+      document.querySelector("#addForm .add").disabled = false;
+      document.getElementById("guest").style.display = "none";
+    } else {
+        document.getElementById('user').style.display = 'none';
+    }
+  document.querySelector(".load").addEventListener("click", loadData);
+
 });
+async function loadData() {
+  const res = await fetch('http://localhost:3030/data/catches');
+  const data=  res.json();
+  console.log(data)
+}
