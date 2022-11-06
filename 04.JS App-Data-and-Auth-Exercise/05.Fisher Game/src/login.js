ffe30c17e8dc005949a005a1548b86bae4a0fcf7
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-06 22:32:01 +0100

    Updating solution for Fisher Game

--- 04.JS App-Data-and-Auth-Exercise/05.Fisher Game/src/login.js
+++ 04.JS App-Data-and-Auth-Exercise/05.Fisher Game/src/login.js
@@ -1,39 +1,36 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form')
-    form.addEventListener('submit', onLogin)
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", onLogin);
+});
 
-})
-async function onLogin(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
+async function onLogin(ev) {
+  ev.preventDefault();
+  const formData = new FormData(ev.target);
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-    const email = formData.get('email');
-    const password = formData.get('password');
-    try {
-
-        const res = await fetch('http://localhost:3030/users/login', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email, password })
-        })
-
-        if (res.ok != true) {
-            const error = await res.json();
-            throw new Error(error.message)
-        }
-        const data = await res.json();
-        const userData = {
-            email: data.email,
-            id: data._id,
-            token: data.accessToken
-        }
-
-        sessionStorage.setItem('userData', JSON.stringify(userData))
-        window.location = '/index.html'
-    } catch (err) {
-        alert(err.message)
+  try {
+    const response = await fetch("http://localhost:3030/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    if (response.ok != true) {
+      const error = await response.json();
+      throw new Error(error.message);
     }
-
+    const data = await response.json();
+    const userData = {
+      email: data.email,
+      id: data._id,
+      token: data.accessToken,
+    };
+    // const token = data.accessToken;
+    sessionStorage.setItem("userData", JSON.stringify(userData));
+    window.location = "index.html";
+  } catch (err) {
+    alert(err.message);
+  }
 }
