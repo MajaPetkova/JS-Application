6c3d24aadea29f3aabc3b735377312c4029f3b3b
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-07 11:13:19 +0100

    Update app.js

--- 04.JS App-Data-and-Auth-Exercise/05.Fisher Game/src/app.js
+++ 04.JS App-Data-and-Auth-Exercise/05.Fisher Game/src/app.js
@@ -1,36 +1,97 @@
+let userData = null;
+
 window.addEventListener("DOMContentLoaded", () => {
-  const userData = JSON.parse(sessionStorage.getItem("userData"));
+  userData = JSON.parse(sessionStorage.getItem("userData"));
   if (userData != null) {
-    document.querySelector("#addForm .add").disabled = false;
     document.getElementById("guest").style.display = "none";
+    document.querySelector("#addForm .add").disabled= false;
   } else {
     document.getElementById("user").style.display = "none";
   }
   document.querySelector(".load").addEventListener("click", loadData);
+
+  document.getElementById("addForm").addEventListener("submit", onCreateSubmit);
 });
+
+async function onCreateSubmit(ev) {
+  ev.preventDefault();
+
+  if(!userData){
+      window.location='login.html'
+    return;
+  }
+ const formData= new FormData(ev.target);
+ const data=[...formData.entries()].reduce((a, [k,v])=> Object.assign(a, {[k]:v }), {});
+//  console.log(data)
+ try{
+    if(Object.values(data).some(x => x == '')){
+    throw new Error('All fields are required')
+    }
+const res= await fetch("http://localhost:3030/data/catches", {
+    method:'post',
+    headers:{
+        'Content-Type':'application/json',
+        'X-Authorization': userData.token
+    },
+    body: JSON.stringify(data)
+});
+   if(res.ok != true){
+    const error= await res.json()
+    throw new Error(error.message)   
+}
+loadData();
+ev.target.reset();
+ }catch(err){
+console.log(err.message)
+ }
+}
+
 async function loadData() {
   const res = await fetch("http://localhost:3030/data/catches");
   const data = await res.json();
-// console.log(data)
-  document.getElementById('catches').replaceChildren(...data.map(createPreview))
+  // console.log(data)
+  document
+    .getElementById("catches")
+    .replaceChildren(...data.map(createPreview));
 }
 
- function createPreview(item) {
+function createPreview(item) {
+  const isOwner = userData && item._ownerId == userData.id;
+
+  //     if(userData && item._ownerId == userData.id){
+  // console.log('current user is owner of item', item.angler)
+  //     }
   const element = document.createElement("div");
   element.className = "catch";
   element.innerHTML = `<label>Angler</label>
-    <input type="text" class="angler" value="${item.angler}">
+    <input type="text" class="angler" value="${item.angler}" ${
+    !isOwner ? "disabled" : ""
+  }>
     <label>Weight</label>
-    <input type="text" class="weight" value="${item.weight}" >
+    <input type="text" class="weight" value="${item.weight}" ${
+    !isOwner ? "disabled" : ""
+  } >
     <label>Species</label>
-    <input type="text" class="species" value="${item.species}" >
+    <input type="text" class="species" value="${item.species}" ${
+    !isOwner ? "disabled" : ""
+  }>
     <label>Location</label>
-    <input type="text" class="location" value="${item.location}" >
+    <input type="text" class="location" value="${item.location}" ${
+    !isOwner ? "disabled" : ""
+  }>
     <label>Bait</label>
-    <input type="text" class="bait" value="${item.bait}">
+    <input type="text" class="bait" value="${item.bait}" ${
+    !isOwner ? "disabled" : ""
+  }>
     <label>Capture Time</label>
-    <input type="number" class="captureTime" value="${item.captureTime}" >
-    <button class="update" data-id="${item._id}" >Update</button>
-    <button class="delete" data-id="${item._id}" >Delete</button>`;
+    <input type="number" class="captureTime" value="${item.captureTime}" ${
+    !isOwner ? "disabled" : ""
+  }>
+    <button class="update" data-id="${item._id}" ${
+    !isOwner ? "disabled" : ""
+  }>Update</button>
+    <button class="delete" data-id="${item._id}" ${
+    !isOwner ? "disabled" : ""
+  }>Delete</button>`;
   return element;
 }
