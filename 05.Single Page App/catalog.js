1db1a7297cce54294288eb4b5f4612e280fa18a1
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-30 15:51:52 +0100

    Refactoring the SAP demo

--- 05.Single Page App/catalog.js
+++ 05.Single Page App/catalog.js
@@ -1,5 +1,17 @@
-import { showSection} from "./dom.js";
+import { showSection, element } from "./dom.js";
 
 export function showCatalogPage() {
   showSection("catalogSection");
+  loadMovies()
+}
+
+async function loadMovies() {
+  const res = await fetch("http://localhost:3030/data/movies");
+  const movies = await res.json();
+  document.querySelector('#catalogSection ul').replaceChildren(...movies.map(createMovieCard))
+  console.log(movies);
+}
+
+function createMovieCard(movie){
+return element('li', {}, movie.title)
 }
--- 05.Single Page App/main.js
+++ 05.Single Page App/main.js
@@ -1,25 +1,24 @@
 // import { showSection } from './dom.js';
 
 import { showAboutPage, showHomePage } from "./home.js";
-import { showCatalogPage } from "./catalog.js"
+import { showCatalogPage } from "./catalog.js";
 // const main = document.querySelector("main");
 document.querySelector("nav").addEventListener("click", onNavigate);
 
-// start application in home view 
+const sections = {
+  homeBtn: showHomePage,
+  catalogBtn: showCatalogPage,
+  aboutBtn: showAboutPage,
+};
+// start application in home view
 showHomePage();
 
 function onNavigate(e) {
-  if (e.target.tagName == "BUTTON") {
-    switch (e.target.id) {
-      case "homeBtn":
-        showHomePage()
-        break;
-      case "catalogBtn":
-        showCatalogPage();
-        break;
-      case "aboutBtn":
-        showAboutPage()
-        break;
-    }
+  if (e.target.tagName == "A") {
+      const view= sections[e.target.id];
+      if(typeof view == 'function'){
+        e.preventDefault();
+          view()
+      }
   }
 }
