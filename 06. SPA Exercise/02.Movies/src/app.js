9d69b97db1c36980be357a2131f137d67c78f756
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-12-05 23:17:59 +0100

    Update login

--- 06. SPA Exercise/02.Movies/src/app.js
+++ 06. SPA Exercise/02.Movies/src/app.js
@@ -14,6 +14,9 @@ const views = {
   loginLink: showLogin,
   registerLink: showRegister,
 };
+const logoutBtn = document
+  .getElementById("logoutBtn")
+  .addEventListener("click", onLogout);
 
 nav.addEventListener("click", (e) => {
   if (e.target.tagName == "A") {
@@ -31,23 +34,36 @@ export function updateNav() {
   const userData = JSON.parse(sessionStorage.getItem("userData"));
   if (userData != null) {
     // console.log([...nav.querySelectorAll(".user")]);
-  nav.querySelector('#welcomeMsg').textContent= `Welcome, ${userData.email}`;
+    nav.querySelector("#welcomeMsg").textContent = `Welcome, ${userData.email}`;
     [...nav.querySelectorAll(".user")].forEach(
       (e) => (e.style.display = "block")
     );
     [...nav.querySelectorAll(".guest")].forEach(
-        (e) => (e.style.display = "none")
-      );
+      (e) => (e.style.display = "none")
+    );
   } else {
     [...nav.querySelectorAll(".user")].forEach(
-        (e) => (e.style.display = "none")
-      );
-      [...nav.querySelectorAll(".guest")].forEach(
-          (e) => (e.style.display = "block")
-        );
+      (e) => (e.style.display = "none")
+    );
+    [...nav.querySelectorAll(".guest")].forEach(
+      (e) => (e.style.display = "block")
+    );
   }
 }
 
+async function onLogout(ev) {
+  ev.preventDefault();
+  ev.stopImmediatePropagation();
+
+  const { token } = JSON.parse(sessionStorage.getItem("userData"));
+  await fetch("http://localhost:3030/users/logout", {
+    headers: {
+      "X-Authorization": token,
+    },
+  });
+  sessionStorage.removeItem("userData");
+  showLogin();
+}
 
 // order of views
 // x catalog (home view)
