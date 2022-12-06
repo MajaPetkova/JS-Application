94717762360bf3b8ba6b6c1f86f1b2d43813a5fe
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-12-06 14:24:34 +0100

    Update details buttons

--- 06. SPA Exercise/02.Movies/src/details.js
+++ 06. SPA Exercise/02.Movies/src/details.js
@@ -11,25 +11,48 @@ section.remove();
 // display logic
 
 export function showDetails(movieId){
-    console.log(movieId)
+    // console.log(movieId)
     showView(section);
     getMovie(movieId)
 }
 
 async function getMovie(id){
-    section.replaceChildren(element('p', {}, 'Loading...'))
-    const res= await fetch('http://localhost:3030/data/movies/' + id);
-    const data= await res.json()
-   
-section.replaceChildren(createDetails(data))
+    section.replaceChildren(element('p', {}, 'Loading...'));
+
+    const requests=[
+
+        fetch('http://localhost:3030/data/movies/' + id),
+        fetch(`http://localhost:3030/data/likes?where=movieId%3D%22${id}%22&distinct=_ownerId&count`), 
+    ]
+    const userData= JSON.parse(sessionStorage.getItem('userData'));
+    if(userData!= null){
+        requests.push( fetch(`/data/likes?where=movieId%3D%22${id}%22%20and%20_ownerId%3D%22${userData.id}%22`))
+    }
+    const [movieRes, likesRes]= await Promise.all(requests)
+    const[movieData, likes, hasLiked]= await Promise.all([
+        movieRes.json(),
+        likesRes.json(),
+        hasLiked && hasLiked.json()
+    ])
+    section.replaceChildren(createDetails(movieData, likes))
 };
 
-function createDetails(movie){
+function createDetails(movie, likes){
     const controls = element('div', {className: "col-md-4 text-center" }, 
     element('h3', {className: 'my-3'}, `Movie description`),
     element('p', {}, movie.description ),
     )
-
+    const userData= JSON.parse(sessionStorage.getItem('userData'));
+    if(userData != null){
+       if(userData.id == movie._ownerId){
+          controls.appendChild(element('a', {className: 'btn btn-danger', href: '#'}, 'Delete'));
+          controls.appendChild(element('a', {className: 'btn btn-warning', href: '#'}, 'Edit'));
+       }else{
+            controls.appendChild(element('a', {className: 'btn btn-primary', href: '#'}, 'Like'));
+       }
+    }
+    controls.appendChild(element('span', {className: 'enrolled-span'}, `Liked: ${likes}`));
+    
     const el= element('div', {className:"container"}, 
               element ('div', {className: "row bg-light text-dark"},
                  element('h1', {},`Movie title: ${movie.title}`),
@@ -42,20 +65,4 @@ function createDetails(movie){
                 
                  return el
 }
-{/* <div class="container">
-<div class="row bg-light text-dark">
-    <h1>Movie title: Black Widow</h1>
 
-    <div class="col-md-8">
-        <img class="img-thumbnail" src="https://miro.medium.com/max/735/1*akkAa2CcbKqHsvqVusF3-w.jpeg" alt="Movie">
-    </div>
-    <div class="col-md-4 text-center">
-        <h3 class="my-3 ">Movie Description</h3>
-        <p>Natasha Romanoff aka Black Widow confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Comes on the screens 2020.</p>
-        <a class="btn btn-danger" href="#">Delete</a>
-        <a class="btn btn-warning" href="#">Edit</a>
-        <a class="btn btn-primary" href="#">Like</a>
-        <span class="enrolled-span">Liked 1</span>
-    </div>
-</div>
-</div> */}
