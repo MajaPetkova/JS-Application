9bd78ca4c3f99432d9953d098f4982f08b4100df
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-12-12 22:45:58 +0100

    added id on nav links and create app.js file

--- 08.SofTerest/src/app.js
+++ 08.SofTerest/src/app.js
@@ -1,3 +1,27 @@
-import * as api from './api/data.js';
+// import * as api from './api/data.js';
+// window.api= api;
 
-window.api= api;
+import { showCatalogPage } from "../views/catalog.js";
+import { showCreatePage } from "../views/create.js";
+import { showDetailsPage } from "../views/details.js";
+import { showHomePage } from "../views/home.js";
+import { showLoginPage } from "../views/login.js";
+import { showRegisterPage } from "../views/register.js";
+
+const links = {
+  homeLink: "home",
+  getStartedLink: "home",
+  catalogLink: "catalog",
+  loginLink: "login",
+  registerLink: "register",
+  createLink: "create",
+};
+
+const views = {
+  home: showHomePage,
+  catalog: showCatalogPage,
+  login: showLoginPage,
+  register: showRegisterPage,
+  create: showCreatePage,
+  details: showDetailsPage
+};
