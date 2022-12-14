9dc80d1a97758613dddb3a6eba472a9546bf3092
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-12-14 10:50:21 +0100

    Added buttons and Event Listeners

--- 09.Client Side Rendering/02.Demowith Lit-Html/app.js
+++ 09.Client Side Rendering/02.Demowith Lit-Html/app.js
@@ -1,23 +1,35 @@
-import {html, render} from 'https://unpkg.com/lit-html?module';
+import { html, render } from "https://unpkg.com/lit-html?module";
 
-
-const articleTemplate=(data)=> html `<article>
-<h3>${data.title}</h3>
-<div class="content-body">
+const articleTemplate = (data) => html`<article>
+  <h3>${data.title}</h3>
+  <div class="content-body">
     <p>${data.content}</p>
-</div>
-<footer>Author: ${data.author} </footer>
-</article>`
+  </div>
+  <footer>Author: ${data.author}</footer>
+</article>`;
 
 async function start() {
   const res = await fetch("./data.json");
   const data = await res.json();
 
-const main= document.querySelector('main');
+  const main = document.querySelector("main");
+  const renderBtn = document.getElementById("renderBtn");
+  renderBtn.addEventListener("click", onRender);
 
-const result= data.map(articleTemplate)
+  document.getElementById("changeBtn").addEventListener('click', onChange)
 
-render(result, main)
-
+  function onRender() {
+    const result = data.map(articleTemplate);
+    render(result, main);
+  }
+  function onChange(){
+    data.shift();
+    data.unshift({
+      "title":"First New Article ",
+      "content":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quaerat vitae harum iusto dignissimos molestiae.",
+      "author":"John Smith"
+    });
+   onRender()
+  }
 }
 start();
