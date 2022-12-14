2ea210a0da9c14690c7d83c896ef04774875036a
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-12-14 13:54:39 +0100

    Adding comments

--- 09.Client Side Rendering/02.Demowith Lit-Html/app.js
+++ 09.Client Side Rendering/02.Demowith Lit-Html/app.js
@@ -1,14 +1,24 @@
 import { html, render } from "https://unpkg.com/lit-html?module";
 
-const articleTemplate = (data) => html`<article>
+const articleTemplate = (onSubmit, data) => html`<article>
   <input type="text" ?disabled=${data.disabled} .value="${data.color}" />
   <h3>${data.title}</h3>
   <div class=${data.color}>
     <p>${data.content}</p>
   </div>
   <footer>Author: ${data.author}</footer>
-  <textarea .value=${'Hello There'}></textarea>
+  <div class="comments">
+    <form @submit=${onSubmit}>
+      <textarea name="comment"></textarea>
+      <button>Submit comment</button>
+    </form>
+
+    <ul>
+      ${data.comments.map(c => html`<li>${c.content}</li>`)}
+   </ul>
+  </div>
 </article>`;
+start();
 
 async function start() {
   const res = await fetch("./data.json");
@@ -21,9 +31,19 @@ async function start() {
   document.getElementById("changeBtn").addEventListener("click", onChange);
 
   function onRender() {
-    const result = data.map(articleTemplate);
+    const result = data.map((a) => articleTemplate(onSubmit.bind(null, a), a));
     render(result, main);
   }
+
+  function onSubmit(article, event) {
+    event.preventDefault();
+    // console.log(article);
+    const formData = new FormData(event.target);
+    const content = formData.get("comment");
+    article.comments.push({ content });
+    // console.log(content);
+  }
+
   function onChange() {
     data.shift();
     data.unshift({
@@ -35,4 +55,3 @@ async function start() {
     onRender();
   }
 }
-start();
