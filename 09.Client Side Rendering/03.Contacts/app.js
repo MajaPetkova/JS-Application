003d414ff71cdcc6f348356ece4b9803f3e5b886
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-12-15 21:46:47 +0100

    Visualizing details

--- 09.Client Side Rendering/03.Contacts/app.js
+++ 09.Client Side Rendering/03.Contacts/app.js
@@ -1,23 +1,36 @@
 import { contacts } from "./contacts.js";
+import { styleMap} from "./node_modules/lit-html/directives/style-map.js";
 import { html, render } from "./node_modules/lit-html/lit-html.js";
 
 start();
 function start() {
   const container = document.getElementById("contacts");
 
-  const contactTemplate = (data) => html`<div class="contact card">
+  const contactTemplate = (data, onDetails) => html`<div class="contact card">
     <div>
       <i class="far fa-user-circle gravatar"></i>
     </div>
     <div class="info">
       <h2>Name: ${data.name}</h2>
-      <button class="detailsBtn" onclick="showDetails(3)">Details</button>
-      <div class="details" id="${data.id}" style="display: none;">
+      <button class="detailsBtn" @click="${() => onDetails(data)}"> ${data.details ? "Hide" : "Details"} </button>
+      <div class="details" id="${data.id}" style = ${styleMap({display: data.details ? "block" : "none" })}>
         <p>Phone number: ${data.phoneNumber}</p>
         <p>Email: ${data.email}</p>
       </div>
     </div>
   </div>`;
 
-  render(contacts.map(contactTemplate), container);
+  onRender();
+
+  function onDetails(contact) {
+    // console.log(contact.id)
+    // contact.details = true;
+    contact.details= !(contact.details);
+    onRender();
+  }
+
+  function onRender() {
+    render(
+      contacts.map((c) => contactTemplate(c, onDetails)), container);
+  }
 }
