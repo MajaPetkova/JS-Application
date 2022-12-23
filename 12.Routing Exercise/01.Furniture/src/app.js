d6914b32e1f0c60f04d2a7d8102468ea82bec48c
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-12-23 15:20:51 +0100

    Added routing

--- 12.Routing Exercise/01.Furniture/src/app.js
+++ 12.Routing Exercise/01.Furniture/src/app.js
@@ -1,11 +1,16 @@
 import { page } from "./lib.js";
 import { catalogPage } from "./views/catalog.js";
+import { createPage } from "./views/create.js";
+import { loginPage } from "./views/login.js";
+import { editPage} from "./views/edit.js";
+import { registerPage } from "./views/register.js";
+import { detailsPage } from "./views/details.js";
 
 page("/", catalogPage);
-page("/details/:id", () => console.log("details"))
-page("/register", () => console.log("register"))
-page("/create", () => console.log("create"))
-page("/login", () => console.log("login"))
-page("/edit/:id", () => console.log("edit"))
-page("/my-furniture", () => console.log("my furniture"))
+page("/details/:id", detailsPage)
+page("/register", registerPage)
+page("/create", createPage)
+page("/login", loginPage)
+page("/edit/:id", editPage)
+page('/my-furniture',catalogPage )
 page.start();
