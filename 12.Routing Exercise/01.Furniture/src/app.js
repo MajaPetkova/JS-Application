296380eeded50bab201d2049b09d53d1decf9494
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-12-23 17:16:56 +0100

    Implement requests

--- 12.Routing Exercise/01.Furniture/src/app.js
+++ 12.Routing Exercise/01.Furniture/src/app.js
@@ -1,16 +1,26 @@
-import { page } from "./lib.js";
+import { page, render } from "./lib.js";
 import { catalogPage } from "./views/catalog.js";
 import { createPage } from "./views/create.js";
 import { loginPage } from "./views/login.js";
-import { editPage} from "./views/edit.js";
+import { editPage } from "./views/edit.js";
 import { registerPage } from "./views/register.js";
 import { detailsPage } from "./views/details.js";
 
+import * as api from "./api/data.js";
+window.api = api;
+const root = document.querySelector("div.container");
+
+page(decorateContext);
 page("/", catalogPage);
-page("/details/:id", detailsPage)
-page("/register", registerPage)
-page("/create", createPage)
-page("/login", loginPage)
-page("/edit/:id", editPage)
-page('/my-furniture',catalogPage )
+page("/details/:id", detailsPage);
+page("/register", registerPage);
+page("/create", createPage);
+page("/login", loginPage);
+page("/edit/:id", editPage);
+page("/my-furniture", catalogPage);
 page.start();
+
+function decorateContext(ctx, next) {
+  ctx.render = (content) => render(content, root);
+  next();
+}
