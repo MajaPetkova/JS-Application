71a031adecdea70c51244b48f6af66baf2faec64
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-12-24 09:30:13 +0100

    Implement fetch request

--- 12.Routing Exercise/01.Furniture/src/views/catalog.js
+++ 12.Routing Exercise/01.Furniture/src/views/catalog.js
@@ -1,6 +1,7 @@
-import { html } from "../lib.js";
+import { getAll } from "../api/data.js";
+import { html, until } from "../lib.js";
 
-const catalogTemplate= ()=> html `
+const catalogTemplate= (dataPromise)=> html `
 <div class="row space-top">
     <div class="col-md-12">
         <h1>Welcome to Furniture System</h1>
@@ -8,51 +9,30 @@ const catalogTemplate= ()=> html `
     </div>
 </div>
 <div class="row space-top">
-    <div class="col-md-4">
-        <div class="card text-white bg-primary">
-            <div class="card-body">
-                    <img src="./images/table.png" />
-                    <p>Description here</p>
-                    <footer>
-                        <p>Price: <span>235 $</span></p>
-                    </footer>
-                    <div>
-                        <a href="/details/123" class="btn btn-info">Details</a>
-                    </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="card text-white bg-primary">
-            <div class="card-body">
-                    <img src="./images/sofa.jpg" />
-                    <p>Description here</p>
-                    <footer>
-                        <p>Price: <span>1200 $</span></p>
-                    </footer>
-                    <div>
-                        <a href="/details/12fghj" class="btn btn-info">Details</a>
-                    </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="card text-white bg-primary">
-            <div class="card-body">
-                    <img src="./images/chair.jpg" />
-                    <p>Description here</p>
-                    <footer>
-                        <p>Price: <span>55 $</span></p>
-                    </footer>
-                    <div>
-                        <a href="/details/123" class="btn btn-info">Details</a>
-                    </div>
-            </div>
-        </div>
-    </div>
+   ${until(dataPromise, html`<p>Loading &hellip; </p>` )}
 </div>`
 
 
+const itemTemplate = (item)=>html
+ `<div class="col-md-4">
+<div class="card text-white bg-primary">
+    <div class="card-body">
+            <img src=${item.img} />
+            <p>${item.description}</p>
+            <footer>
+                <p>Price: <span>${item.price} $</span></p>
+            </footer>
+            <div>
+                <a href=${`/details/${item._id}`} class="btn btn-info">Details</a>
+            </div>
+    </div>
+</div>
+`
+
 export function catalogPage (ctx) {
-ctx.render(catalogTemplate())
+ctx.render(catalogTemplate(loadItems()))
+}
+async function loadItems(){
+   const items = await getAll();
+   return items.map(itemTemplate)
 }
