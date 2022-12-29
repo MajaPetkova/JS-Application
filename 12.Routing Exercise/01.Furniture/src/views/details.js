1342b55f9e4e2fe43f24722b63917d006007d309
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-12-29 11:03:19 +0100

    Added delete for one item

--- 12.Routing Exercise/01.Furniture/src/views/details.js
+++ 12.Routing Exercise/01.Furniture/src/views/details.js
@@ -1,5 +1,6 @@
-import { getById } from "../api/data.js";
+import { deleteItem, getById } from "../api/data.js";
 import { html, until } from "../lib.js";
+import { getUserData } from "../util.js";
 
 
 const detailsTemplate = (itemPromise)=> html`  
@@ -12,7 +13,7 @@ const detailsTemplate = (itemPromise)=> html`
  ${until (itemPromise, html `Loading &hellip;`)}
 </div>`;
 
-const itemTemplate=(item)=> html `<div class="col-md-4">
+const itemTemplate=(item, isOwner, onDelete)=> html `<div class="col-md-4">
 <div class="card text-white bg-primary">
     <div class="card-body">
         <img src=${item.img} />
@@ -26,18 +27,30 @@ const itemTemplate=(item)=> html `<div class="col-md-4">
 <p>Description: <span>${item.description}</span></p>
 <p>Price: <span>${item.price}</span></p>
 <p>Material: <span>${item.material}</span></p>
-<div>
+${isOwner ? html`<div>
     <a href=${`edit/${item._id}`} class="btn btn-info">Edit</a>
-    <a href="javascript:void(0)" class="btn btn-red">Delete</a>
-</div>
+    <a @click=${onDelete} href="javascript:void(0)" class="btn btn-red">Delete</a>
+</div>` : null}
+
 </div>`;
 
 export function detailsPage (ctx) {
-    ctx.render(detailsTemplate(loadItem(ctx.params.id)))
-//  console.log("details", ctx.params.id )
+    ctx.render(detailsTemplate(loadItem(ctx.params.id, onDelete)));
+
+    async function onDelete(){
+     const choice= confirm("Are you sure you want to delete this item?");
+
+     if(choice){
+        await deleteItem(ctx.params.id);
+        ctx.page.redirect('/')
+     }
+    }
 }
 
-async function loadItem(id){
+async function loadItem(id, onDelete){
  const item= await getById(id);
-return itemTemplate(item)
+
+ const userData= getUserData();
+ const isOwner=userData.id == item._ownerId;
+return itemTemplate(item, isOwner, onDelete)
 }
