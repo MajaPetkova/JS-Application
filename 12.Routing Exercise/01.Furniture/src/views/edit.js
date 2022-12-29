52b6600cf2ea63e75a24fd2172d7f2bdb3a461eb
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-12-29 12:33:10 +0100

    Added error handling for edit

--- 12.Routing Exercise/01.Furniture/src/views/edit.js
+++ 12.Routing Exercise/01.Furniture/src/views/edit.js
@@ -12,37 +12,38 @@ ${until (itemPromise, html `<p>Loading &hellip;</p>` )}
 `
 const formTemplate = (item, onSubmit, errorMsg, errors) => html `
 <form @submit= ${onSubmit}>
+${errorMsg ? html `<div class= "form-group error">${errorMsg}</div>` : null }
 <div class="row space-top">
     <div class="col-md-4">
         <div class="form-group">
             <label class="form-control-label" for="new-make">Make</label>
-            <input class="form-control" id="new-make" type="text" name="make" .value=${item.make}>
+            <input class=${'form-control' + (errors.make ? " is-invalid" : "" )} id="new-make" type="text" name="make" .value=${item.make}>
         </div>
         <div class="form-group has-success">
             <label class="form-control-label" for="new-model">Model</label>
-            <input class="form-control is-valid" id="new-model" type="text" name="model" .value=${item.model}>
+            <input class=${'form-control' + (errors.model ? " is-invalid" : "" )} id="new-model" type="text" name="model" .value=${item.model}>
         </div>
         <div class="form-group has-danger">
             <label class="form-control-label" for="new-year">Year</label>
-            <input class="form-control is-invalid" id="new-year" type="number" name="year" .value=${item.year}>
+            <input class=${'form-control' + (errors.year ? " is-invalid" : "" )} id="new-year" type="number" name="year" .value=${item.year}>
         </div>
         <div class="form-group">
             <label class="form-control-label" for="new-description">Description</label>
-            <input class="form-control" id="new-description" type="text" name="description" .value=${item.description}>
+            <input class=${'form-control' + (errors.description ? " is-invalid" : "" )} id="new-description" type="text" name="description" .value=${item.description}>
         </div>
     </div>
     <div class="col-md-4">
         <div class="form-group">
             <label class="form-control-label" for="new-price">Price</label>
-            <input class="form-control" id="new-price" type="number" name="price" .value=${item.price}>
+            <input class=${'form-control' + (errors.price ? " is-invalid" : "" )} id="new-price" type="number" name="price" .value=${item.price}>
         </div>
         <div class="form-group">
             <label class="form-control-label" for="new-image">Image</label>
-            <input class="form-control" id="new-image" type="text" name="img" .value=${item.img}>
+            <input class=${'form-control' + (errors.img ? " is-invalid" : "" )} id="new-image" type="text" name="img" .value=${item.img}>
         </div>
         <div class="form-group">
             <label class="form-control-label" for="new-material">Material (optional)</label>
-            <input class="form-control" id="new-material" type="text" name="material" .value=${item.material}>
+            <input class=${'form-control' + (errors.material ? " is-invalid" : "" )} id="new-material" type="text" name="material" .value=${item.material}>
         </div>
         <input type="submit" class="btn btn-info" value="Edit" />
     </div>
@@ -53,15 +54,15 @@ const formTemplate = (item, onSubmit, errorMsg, errors) => html `
 export function editPage (ctx) {
     const itemPromise = getById(ctx.params.id);
     
-    update(null, {});
+    update(itemPromise, null, {});
 
-    function update(errorMsg, errors){
-        ctx.render(editTemplate(loadItem(itemPromise)))
+    function update(itemPromise, errorMsg, errors){
+        ctx.render(editTemplate(loadItem(itemPromise, errorMsg, errors)))
     }
 
-    async function loadItem(itemPromise){
+    async function loadItem(itemPromise, errorMsg, errors){
     const item=await itemPromise;
-    return formTemplate(item, onSubmit, "", {})
+    return formTemplate(item, onSubmit,errorMsg, errors)
     }
 
     async function onSubmit(ev){
@@ -88,7 +89,7 @@ export function editPage (ctx) {
     
           } catch(err){
             const message = err.message || err.error.message;
-            update(message, err.errors || {})
+            update(data, message, err.errors || {})
           }
         }
     }
