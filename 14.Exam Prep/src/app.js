77328f50228af6c1313b5dc1d105c09ae9e2b5d9
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-08 20:15:54 +0100

    Added lib.js  and home template and update app.js

--- 14.Exam Prep/src/app.js
+++ 14.Exam Prep/src/app.js
@@ -1,3 +1,12 @@
+import { page } from "./lib.js";
+import { homePage } from "./views/home.js";
 
-import * as api from "./api/api.js";
-window.api= api;
+// import * as api from "./api/data.js";
+// window.api= api;
+
+const root= document.querySelector("main")
+
+page('/', homePage);
+page('/memes', catalogPage);
+page.start();
+
