752e07734d8e370002d5fb3cdc4c8306eee380b2
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-09 17:12:44 +0100

    Added login page

--- 14.Exam Prep/src/app.js
+++ 14.Exam Prep/src/app.js
@@ -1,6 +1,7 @@
 import { page, render } from "./lib.js";
 import { catalogPage } from "./views/catalog.js";
 import { homePage } from "./views/home.js";
+import { loginPage } from "./views/login.js";
 
 // import * as api from "./api/data.js";
 // window.api= api;
@@ -10,6 +11,7 @@ const root = document.querySelector("main");
 page(decorateContext)
 page("/", homePage);
 page("/memes", catalogPage);
+page("/login", loginPage)
 page.start();
 
 function decorateContext(ctx, next) {
--- 14.Exam Prep/src/views/catalog.js
+++ 14.Exam Prep/src/views/catalog.js
@@ -8,13 +8,13 @@ const catalogTemplate = (memes) => html`<section id="meme-feed">
       ? html`<p class="no-memes">No memes in database.</p>`
       : memes.map(memeCard)}
   </div>
-</section> `;
+</section>`;
 
 const memeCard = (meme) => html` <div class="meme">
   <div class="card">
     <div class="info">
       <p class="meme-title">${meme.title}</p>
-      <img class="meme-image" alt="meme-img" src=${meme.imageUrl} />
+      <img class="meme-image" alt="meme-img" src=${meme.imageUrl}/>
     </div>
     <div id="data-buttons">
       <a class="button" href="/details/${meme._id}">Details</a>
@@ -24,6 +24,5 @@ const memeCard = (meme) => html` <div class="meme">
 
 export async function catalogPage(ctx) {
   const memes = await getAllMemes();
-  console.log("it works")
   ctx.render(catalogTemplate(memes));
 }
