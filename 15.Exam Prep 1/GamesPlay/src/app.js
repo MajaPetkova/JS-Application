bbd8f45e3db0df0bf32b551403785314c614b683
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-15 18:36:28 +0100

    Added logout and update user navigation

--- 15.Exam Prep 1/GamesPlay/src/app.js
+++ 15.Exam Prep 1/GamesPlay/src/app.js
@@ -1,18 +1,41 @@
-import {page, render} from "./lib.js";
+import { logout } from "./api/data.js";
+import { page, render } from "./lib.js";
+import { getUserData } from "./util.js";
 import { homePage } from "./views/home.js";
 import { loginPage } from "./views/login.js";
 
 // import * as api from "./api/data.js";
 // window.api= api;
-const root= document.getElementById("main-content");
+const root = document.getElementById("main-content");
+document.getElementById("logoutBtn").addEventListener("click", onLogout);
 
-page(decorateContext)
+
+updateUserNav();
+page(decorateContext);
 page("/home", homePage);
-page("/login", loginPage )
+page("/login", loginPage);
+
 page.start();
 
+function decorateContext(ctx, next) {
+  ctx.render = (content) => render(content, root);
+  ctx.updateUserNav = updateUserNav;
+  next();
+}
 
-function decorateContext(ctx, next){
-    ctx.render = (content) => render(content, root)
-    next()
+export function updateUserNav() {
+  const userData = getUserData();
+  if (userData) {
+    document.getElementById("user").style.display = "block";
+    document.getElementById("guest").style.display = "none";
+  } else {
+    document.getElementById("user").style.display = "none";
+    document.getElementById("guest").style.display = "block";
+  }
+}
+
+async function onLogout(){
+    logout();
+    updateUserNav();
+    page.redirect("/home")
 }
