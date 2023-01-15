ebcf6923d224e6198e9e579230f6d52ae083505c
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-15 17:19:07 +0100

    Added home template

--- 15.Exam Prep 1/GamesPlay/src/app.js
+++ 15.Exam Prep 1/GamesPlay/src/app.js
@@ -1,4 +1,16 @@
 import {page, render} from "./lib.js";
+import { homePage } from "./views/home.js";
 
-import * as api from "./api/data.js";
-window.api= api;
+// import * as api from "./api/data.js";
+// window.api= api;
+const root= document.getElementById("main-content");
+
+page(decorateContext)
+page("/", homePage);
+page.start();
+
+
+function decorateContext(ctx, next){
+    ctx.render = (content) => render(content, root)
+    next()
+}
