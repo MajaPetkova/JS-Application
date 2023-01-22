96ecb61a0b2ab006e5098e6c7c6105a4af2ec8ee
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-22 23:01:05 +0100

    Added register template and functionality

--- 20.Cosmetic Kingdom/src/app.js
+++ 20.Cosmetic Kingdom/src/app.js
@@ -1,17 +1,19 @@
 import{page, render} from "./lib.js"
+// import * as api from "./api/data.js"
+// window.api= api;
 
 import { dashboardPage } from "./views/dashboard.js";
 import { homePage } from "./views/home.js";
-import * as api from "./api/data.js"
 import { loginPage } from "./views/login.js";
-window.api= api;
+import { registerPage } from "./views/register.js";
 
 const root= document.querySelector("main")
 
 page(decorateContext);
 page("/", homePage)
 page("/dashboard", dashboardPage);
-page("/login", loginPage)
+page("/login", loginPage);
+page("/register", registerPage)
 page.start();
 
 
---
+++ 20.Cosmetic Kingdom/src/views/register.js
@@ -0,0 +1,48 @@
+import { register } from "../api/data.js";
+import { html, render } from "../lib.js";
+
+const registerTemplate = (onSubmit) => html` <section id="register">
+  <div class="form">
+    <h2>Register</h2>
+    <form @submit=${onSubmit}class="register-form">
+      <input type="text" name="email" id="register-email" placeholder="email" />
+      <input
+        type="password"
+        name="password"
+        id="register-password"
+        placeholder="password"
+      />
+      <input
+        type="password"
+        name="re-password"
+        id="repeat-password"
+        placeholder="repeat password"
+      />
+      <button type="submit">register</button>
+      <p class="message">Already registered? <a href="/login">Login</a></p>
+    </form>
+  </div>
+</section>`;
+
+export async function registerPage(ctx) {
+  ctx.render(registerTemplate(onSubmit));
+
+  async function onSubmit(ev) {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.target);
+    const email = formData.get("email").trim();
+    const password = formData.get("password").trim();
+    const repass = formData.get("re-password").trim();
+
+    if (email == "" || password == "") {
+      return alert("All fields are required");
+    }
+    if (password !== repass) {
+      return alert("Passwords don't match ");
+    }
+
+    await register(email, password);
+    ctx.page.redirect("/dashboard");
+  }
+}
