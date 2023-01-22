fa5b09eb22f1fe947cee261c9ceb25d6f86ad9b5
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-22 22:44:43 +0100

    Added login template and functionality

--- 20.Cosmetic Kingdom/src/app.js
+++ 20.Cosmetic Kingdom/src/app.js
@@ -3,13 +3,15 @@ import{page, render} from "./lib.js"
 import { dashboardPage } from "./views/dashboard.js";
 import { homePage } from "./views/home.js";
 import * as api from "./api/data.js"
+import { loginPage } from "./views/login.js";
 window.api= api;
 
 const root= document.querySelector("main")
 
 page(decorateContext);
 page("/", homePage)
-page("/dashboard", dashboardPage)
+page("/dashboard", dashboardPage);
+page("/login", loginPage)
 page.start();
 
 
--- 20.Cosmetic Kingdom/src/views/dashboard.js
+++ 20.Cosmetic Kingdom/src/views/dashboard.js
@@ -1,28 +1,12 @@
 import { getAllProducts } from "../api/data.js";
-import {html, render} from "../lib.js";
+import {html} from "../lib.js";
 // const main= document.querySelector("main")
 
 const dashboardTemplate= (products)=> html `<h2>Products</h2>
 <section id="dashboard">
   <!-- Display a div with information about every post (if any)-->
  ${products.length == 0 ? html `<h2>No products yet.</h2>` : products.map(productCard)}
-  <!-- <div class="product">
-    <img src="./images/product example 2.png" alt="example1" />
-    <p class="title">
-    Eyeshadow Palette
-    </p>
-    <p><strong>Price:</strong><span class="price">19.99</span>$</p>
-    <a class="details-btn" href="">Details</a> -->
-  <!-- </div><div class="product">
-    <img src="./images/product example 3.png" alt="example1" />
-    <p class="title">
-    Lipstick
-    </p>
-    <p><strong>Price:</strong><span class="price">16.99 </span>$</p>
-    <a class="details-btn" href="">Details</a>
-  </div> -->
 </section>
- <!-- Display an h2 if there are no posts -->
  `
 
 const productCard= (product)=> html ` <div class="product">
