79139205acf5c95c03b066825a788371f2382d70
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-23 09:55:04 +0100

    Guest cannot see buy button

--- 20.Cosmetic Kingdom/src/views/details.js
+++ 20.Cosmetic Kingdom/src/views/details.js
@@ -2,7 +2,7 @@ import { deleteProduct, getProductById } from "../api/data.js";
 import { html } from "../lib.js";
 import { getUserData } from "../util.js";
 
-const detailsTemplate = (product, isOwner, onDelete) => html` <section
+const detailsTemplate = (product, isOwner, isGuest, onDelete) => html` <section
   id="details"
 >
   <div id="details-wrapper">
@@ -22,26 +22,28 @@ const detailsTemplate = (product, isOwner, onDelete) => html` <section
     </div>
 
     <div id="action-buttons">
-      ${isOwner
+      ${isGuest ? html `${isOwner
         ? html`<a href="/edit/${product._id}" id="edit-btn">Edit</a>
             <a @click=${onDelete} href="" id="delete-btn">Delete</a>`
-        : html` <a href="" id="buy-btn">Buy</a>`}
-    </div>
-  </div>
+        : html` <a href="" id="buy-btn">Buy</a>`}`
+        : null
+      }
+      </div>
 </section>`;
 
 export async function detailsPage(ctx) {
   const product = await getProductById(ctx.params.id);
   const userData = getUserData();
   const isOwner = userData && userData.id == product._ownerId;
-  ctx.render(detailsTemplate(product, isOwner, onDelete));
+  const isGuest = userData && userData.id !== product._ownerId;
+  ctx.render(detailsTemplate(product, isOwner, isGuest, onDelete));
 
   async function onDelete() {
     const choice = confirm("Are you sure you want to delete this product?");
 
     if (choice) {
       await deleteProduct(ctx.params.id);
-      ctx.page.redirect("/dashboard")
+      ctx.page.redirect("/dashboard");
     }
   }
 }
