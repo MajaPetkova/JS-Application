d50a57f835105ab7276f8ac176e3029c587a6686
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-24 17:08:02 +0100

    Added solution for quiz app

--- 21.Quizz App/script.js
+++ 21.Quizz App/script.js
@@ -37,33 +37,64 @@ const quizData = [
     b: "2001",
     c: "1999",
     d: "1995",
-    correct : "d"
-},
+    correct: "d",
+  },
 ];
 
-const questionEl= document.getElementById("question");
-const a_text= document.getElementById("a_text");
-const b_text= document.getElementById("b_text");
-const c_text= document.getElementById("c_text");
-const d_text= document.getElementById("d_text");
-const submitBtn= document.querySelector("button");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.querySelector("button");
+const quiz = document.querySelector(".quiz-container");
+const answers = document.querySelectorAll(".answer");
 
-let currentQuestion= 0;
+let currentQuestion = 0;
+let score = 0;
 loadQuiz();
 
-function loadQuiz(){
-    questionEl.innerHTML= quizData[currentQuestion].question;
-    a_text.innerHTML= quizData[currentQuestion].a;
-    b_text.innerHTML= quizData[currentQuestion].b;
-    c_text.innerHTML= quizData[currentQuestion].c;
-    d_text.innerHTML= quizData[currentQuestion].d;
-    
+function loadQuiz() {
+  deselectAnswers();
+  questionEl.innerHTML = quizData[currentQuestion].question;
+  a_text.innerHTML = quizData[currentQuestion].a;
+  b_text.innerHTML = quizData[currentQuestion].b;
+  c_text.innerHTML = quizData[currentQuestion].c;
+  d_text.innerHTML = quizData[currentQuestion].d;
 }
-submitBtn.addEventListener("click", ()=> {
-    currentQuestion++;
-    if(currentQuestion < quizData.length){
-        loadQuiz()
-    }else{
-        alert ("You are finished!")
+
+function getSelected() {
+  let answer = undefined;
+
+  answers.forEach((x) => {
+    if (x.checked) {
+      answer = x.id;
     }
-})
+  });
+  return answer;
+}
+
+function deselectAnswers() {
+  answers.forEach((x) => {
+    x.checked = false;
+  });
+}
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuestion].correct) {
+      score++;
+    }
+
+    currentQuestion++;
+    // console.log(answer);
+    if (currentQuestion < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `<h2>Your score is ${score}/${quizData.length} </h2>
+    <button onclick="location.reload()"}>Reload</button>`;
+      // alert("You are finished!");
+    }
+  }
+});
