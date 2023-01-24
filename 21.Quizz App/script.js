6547eaf1ce0825dd1b2a7141ffe72221e3afe06f
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-24 15:37:42 +0100

    Getting elements and add event listener on button

--- 21.Quizz App/script.js
+++ 21.Quizz App/script.js
@@ -40,3 +40,30 @@ const quizData = [
     correct : "d"
 },
 ];
+
+const questionEl= document.getElementById("question");
+const a_text= document.getElementById("a_text");
+const b_text= document.getElementById("b_text");
+const c_text= document.getElementById("c_text");
+const d_text= document.getElementById("d_text");
+const submitBtn= document.querySelector("button");
+
+let currentQuestion= 0;
+loadQuiz();
+
+function loadQuiz(){
+    questionEl.innerHTML= quizData[currentQuestion].question;
+    a_text.innerHTML= quizData[currentQuestion].a;
+    b_text.innerHTML= quizData[currentQuestion].b;
+    c_text.innerHTML= quizData[currentQuestion].c;
+    d_text.innerHTML= quizData[currentQuestion].d;
+    
+}
+submitBtn.addEventListener("click", ()=> {
+    currentQuestion++;
+    if(currentQuestion < quizData.length){
+        loadQuiz()
+    }else{
+        alert ("You are finished!")
+    }
+})
