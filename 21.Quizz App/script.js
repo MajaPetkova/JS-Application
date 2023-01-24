4de095cc8d1727aff9c5e67905ce6c6af191245e
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-24 14:57:41 +0100

    Added  new task - Quiz App

---
+++ 21.Quizz App/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: "How old is the Admin?",
+    a: "30",
+    b: "27",
+    c: "29",
+    d: "32",
+    correct: "c",
+  },
+  {
+    question: "What is the most used programming language in 2019?",
+    a: "C",
+    b: "Java",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question: "Who is the president of US",
+    a: "J. Biden",
+    b: "D.Trump",
+    c: "J. Trudeau",
+    d: "V.Putin",
+    correct: "a",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "2001",
+    c: "1999",
+    d: "1995",
+    correct : "d"
+},
+];
