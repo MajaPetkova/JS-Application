c149a959e9544bff90e4091921d1b6d3c1542498
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-25 11:03:35 +0100

    Added EventListener on the button
    Add and remove item from local storage

--- 22.Recipe App/index.js
+++ 22.Recipe App/index.js
@@ -8,18 +8,20 @@ async function getRandomMeal() {
   const randomMeal = data.meals[0];
 
   addMeal(randomMeal, true);
-   console.log(randomMeal)
 }
+
 // function getMealById(id) {
 //   fetch("http://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
 // }
 // function getMealsBySearch(term) {
 //   fetch("http://www.themealdb.com/api/json/v1/1/search.php?s=" + term);
 // }
+
 function addMeal(mealData, random = false) {
-    const meal= document.createElement("div");
-    meal.classList.add("meal");
-    meal.innerHTML= `<div class="meal-header">
+  console.log(mealData);
+  const meal = document.createElement("div");
+  meal.classList.add("meal");
+  meal.innerHTML = `<div class="meal-header">
    ${random ? ` <span class="random">Random recipe</span>` : ""}
     <img src="${mealData.strMealThumb}" alt="${mealData.Meal}">
 </div>
@@ -29,5 +31,37 @@ function addMeal(mealData, random = false) {
         <i class="fa fa-heart"></i>
       </button>
 </div>`;
-meals.appendChild(meal)
+  meals.appendChild(meal);
+
+  meal.querySelector(".meal-body .fav-btn").addEventListener("click", (ev) => {
+    if (ev.target.classList.contains("active")) {
+      removeMealFromLS(mealData.idMeal);
+      ev.target.classList.remove("active");
+    } else {
+      addMealToLS(mealData.idMeal);
+      ev.target.classList.add("active");
+    }
+  });
+}
+
+function addMealToLS(mealId) {
+  const mealIds = getMealsFromLS();
+
+  localStorage.setItem("mealIds", JSON.stringify([...mealIds, mealId]));
+  // console.log(mealsIds)
+}
+
+function removeMealFromLS(mealId) {
+  const mealIds = getMealsFromLS();
+
+  localStorage.setItem(
+    "mealIds",
+    JSON.stringify(mealIds.filter((id) => id !== mealId))
+  );
+}
+
+function getMealsFromLS() {
+  const mealIds = JSON.parse(localStorage.getItem("mealsIds"));
+  console.log(mealIds);
+  return mealIds == null ? [] : mealIds;
 }
