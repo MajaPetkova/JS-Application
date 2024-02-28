0196ee6e60a19e10d6f932510e67787dd2d3691e
Maja Petkova <majapetkova1@yahoo.com>
2024-02-28 23:45:22 +0100

    Add animation for the input field and update condition for the input

--- 27.QR Generator/index.js
+++ 27.QR Generator/index.js
@@ -2,14 +2,19 @@ const url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=`;
 
 let imgBox = document.querySelector(".imgBox");
 let qrImage = document.getElementById("qrImage");
-let qrInput = document.getElementById("qrInput").value;
+let qrInput = document.getElementById("qrInput");
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", generateQR);
 
 function generateQR() {
-    if(qrInput.length > 0){
-        qrImage.src = url + qrInput;
-        imgBox.classList.add("showImg")
-    }
+  if (qrInput.value.length > 0) {
+    qrImage.src = url + qrInput.value;
+    imgBox.classList.add("showImg");
+  } else {
+    qrInput.classList.add("error");
+    setTimeout(() => {
+      qrInput.classList.remove("error");
+    }, 1000);
+  }
 }
