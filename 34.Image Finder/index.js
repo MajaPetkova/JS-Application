ef142a87e1c60c5a930b855b948c8c9510ba6a6f
Maja Petkova <majapetkova1@yahoo.com>
2024-06-04 23:30:20 +0200

    Update style and add script

--- 34.Image Finder/index.js
+++ 34.Image Finder/index.js
@@ -1,1 +1,47 @@
-let accessKey="4B75IMQ9mv-WdZrZMpukN9mZFiAYnd0FlXOd5N6hSRQ"
+let accessKey = "4B75IMQ9mv-WdZrZMpukN9mZFiAYnd0FlXOd5N6hSRQ";
+
+let searchForm = document.getElementById("searchForm");
+let searchBox = document.getElementById("search-box");
+let searchResult = document.getElementById("search-result");
+let showMoreBtn = document.getElementById("show-more-btn");
+
+let keyWord = "";
+let page = 1;
+
+async function searchImg() {
+  keyWord = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyWord}&client_id=${accessKey}&per_page=12`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (page === 1) {
+    searchResult.innerHTML = "";
+    searchBox.value=""
+  }
+
+  const results = data.results;
+  results.map((x) => {
+    const image = document.createElement("img");
+    image.src = x.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = x.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+
+    searchResult.appendChild(imageLink);
+  });
+  showMoreBtn.style.display = "block";
+}
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImg();
+});
+
+showMoreBtn.addEventListener("click", () => {
+  page++;
+  searchImg();
+});
